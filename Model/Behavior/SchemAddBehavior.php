4a74260b6b576fca52698a998047da33683258af
Ceeram <c33ram@gmail.com>
2011-09-03 16:34:58 +0200

    fixing behavior and added README

--- Model/Behavior/SchemAddBehavior.php
+++ Model/Behavior/SchemAddBehavior.php
@@ -12,28 +12,35 @@ class SchemAddBehavior extends ModelBehavior {
  * @param array $config
  * @return void
  */
-	public function setup($model, $extra = array()) {
-		if (!empty($model->schema)) {
-			$extra = array_merge($extra, $model->schema);
-		}
-		if (!$extra) {
-			return;
-		}
+    public function setup($model, $extra = array()) {
+        if (!empty($model->schema)) {
+            $extra = array_merge($extra, $model->schema);
+        }
+        $this->settings[$model->name] = array('extra' => $extra);
 
-		$_schema = $model->schema();
-		if (!$_schema) {
-			return;
-		}
+    }
 
-		foreach($_schema as $field => $meta) {
-			if (array_key_exists($field, $extra) && is_array($extra[$field])) {
-				foreach ($extra[$field] as $extraKey => $extraValue) {
-					if (!isset($_schema[$field][$extraKey])) {
-						$_schema[$field][$extraKey] = $extraValue;
-					}
-				}
-			}
-		}
-	}
+/**
+ * Sets up the configuration for the model
+ *
+ * @param array $config
+ * @return void
+ */
+    public function addTitles($model, $_schema = array()) {
+        $extra = $this->settings[$model->name]['extra'];
+        if (!$extra) {
+            return $_schema;
+        }
+        foreach($_schema as $field => $meta) {
+            if (array_key_exists($field, $extra) && is_array($extra[$field])) {
+                foreach ($extra[$field] as $extraKey => $extraValue) {
+                    if (!isset($_schema[$field][$extraKey])) {
+                        $_schema[$field][$extraKey] = $extraValue;
+                    }
+                }
+            }
+        }
+        return $_schema;
+    }
 
 }
